93f262c18b498cba6d1dca0339b12e787e28e115
mhnhai <156775752+mhnhai@users.noreply.github.com>
2024-11-01 16:40:22 +0700

    update

--- frontend/src/components/SignUpForm.vue
+++ frontend/src/components/SignUpForm.vue
@@ -1,6 +1,13 @@
 <template>
   <Form @submit="submitAccount" :validation-schema="accountFormSchema">
     <div class="form-group">
+      <label for="fullname">Họ và tên</label>
+
+      <Field name="fullname" type="text" class="form-control" v-model="accountLocal.fullname" />
+
+      <ErrorMessage name="fullname" class="error-feedback" />
+    </div>
+    <div class="form-group">
       <label for="username">Tên tài khoản</label>
 
       <Field name="username" type="text" class="form-control" v-model="accountLocal.username" />
@@ -8,6 +15,13 @@
       <ErrorMessage name="username" class="error-feedback" />
     </div>
     <div class="form-group">
+      <div class="form-group">
+        <label for="email">Email</label>
+
+        <Field name="email" type="email" class="form-control" v-model="accountLocal.email" />
+
+        <ErrorMessage name="email" class="error-feedback" />
+      </div>
       <label for="password">Mật khẩu</label>
 
       <Field name="password" type="password" class="form-control" v-model="accountLocal.password" />
@@ -15,11 +29,42 @@
       <ErrorMessage name="password" class="error-feedback" />
     </div>
     <div class="form-group">
-      <label for="email">Email</label>
+      <label for="password">Nhập lại mật khẩu</label>
 
-      <Field name="email" type="email" class="form-control" v-model="accountLocal.email" />
+      <Field name="password2" type="password" class="form-control" />
 
-      <ErrorMessage name="email" class="error-feedback" />
+      <ErrorMessage name="password2" class="error-feedback" />
+    </div>
+    <div class="form-group">
+      <label for="gender">Giới tính</label>
+      <div class="col-md-4">
+        <select name="gender" class="form-select" v-model="accountLocal.gender">
+          <option value="male">Nam</option>
+          <option value="female">Nữ</option>
+        </select>
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="phone">Số điện thoại</label>
+
+      <Field name="phone" type="text" class="form-control" v-model="accountLocal.phone" />
+
+      <ErrorMessage name="phone" class="error-feedback" />
+    </div>
+    <div class="form-group">
+      <label for="birthdate">Sinh nhật</label>
+
+      <Field name="birthdate" type="date" class="form-control" v-model="accountLocal.birthdate" />
+
+      <ErrorMessage name="birthdate" class="error-feedback" />
+    </div>
+
+    <div class="form-group">
+      <label for="address">Địa chỉ</label>
+
+      <Field name="address" type="text" class="form-control" v-model="accountLocal.address" />
+
+      <ErrorMessage name="address" class="error-feedback" />
     </div>
     <div class="form-group d-flex justify-content-center mt-3">
       <button v-if="!accountLocal._id" class="btn btn-primary btn-sm">Tạo tài khoản</button>
@@ -47,18 +92,28 @@ export default {
   },
   data() {
     const accountFormSchema = yup.object().shape({
+      fullname: yup.string().required("Nhập họ và tên."),
       username: yup
         .string()
-        .required("Tên phải có giá trị.")
+        .required("Nhập tên tài khoản.")
         .min(2, "Tên phải ít nhất 2 ký tự.")
         .max(50, "Tên có nhiều nhất 50 ký tự."),
       email: yup
         .string()
+        .required("Nhập email.")
         .email("E-mail không đúng.")
         .max(50, "E-mail tối đa 50 ký tự."),
       password: yup
         .string()
-        .min(8, "Mật khẩu phải ít nhất 8 ký tự.")
+        .required("Nhập mật khẩu.")
+        .min(8, "Mật khẩu phải ít nhất 8 ký tự."),
+      password2: yup
+        .string()
+        .required("Nhập lại mật khẩu")
+        .oneOf([yup.ref('password')], 'Mật khẩu không trùng khớp'),
+      gender: yup
+        .string()
+        .required()
     });
     return {
       // Chúng ta sẽ không muốn hiệu chỉnh props, nên tạo biến cục bộ
